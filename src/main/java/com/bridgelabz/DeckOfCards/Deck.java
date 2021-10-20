e617617172f43b02ee257ea6d9d700551d302dcd
Admin <sandhyabagale10@gmail.com>
2021-10-20 19:23:57 +0530

    Added:welcome message

---
+++ src/main/java/com/bridgelabz/DeckOfCards/Deck.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.DeckOfCards;
+
+import java.util.ArrayList;
+
+public class Deck {
+	public static void main(String[] args) {
+		System.out.println("welcome to DeckOf Cards program...!");
+	}
+}
